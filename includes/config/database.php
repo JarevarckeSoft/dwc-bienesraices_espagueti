3c603b0d1f015fffa566d18faf717da879ca6d38
Keren Arevalo (Jarevarcke) <jarevarcke@gmail.com>
2024-04-01 16:30:37 -0600

    Modificación a algunos archivos

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarDB() : mysqli {
-    $db = mysqli_connect('localhost', 'root', 'rootadmin', 'bienesraices_crud');
+    $db = mysqli_connect('localhost', 'root', 'root', 'bienesraices_crud');
 
     if (!$db) {
         echo "Error en la conexión a la BD";
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION)) {
 $auth = $_SESSION['login'] ?? false;
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
